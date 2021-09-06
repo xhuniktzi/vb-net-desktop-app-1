50d7aeeeb4d5e5098fc7eb3e12ccb4586e5528a4
Xhunik Miguel <xhuniktzi@gmail.com>
2021-09-06 17:44:47 -0600

    refresh button

--- Forms/ProductForm.vb
+++ Forms/ProductForm.vb
@@ -9,15 +9,27 @@
         _viewModel = New ProductMainViewModel(New ProductRepository())
     End Sub
     Private Sub ProductForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadData()
+    End Sub
+
+    Private Sub LoadData()
         _viewModel.LoadProducts()
         BindingSourceProducts.DataSource = _viewModel.Products
         LstBoxListProducts.DataSource = BindingSourceProducts
         LstBoxListProducts.DisplayMember = "ProductName"
+        If TxtBoxProductCode.DataBindings.Count > 0 Then
+            BindingSourceProducts.ResetBindings(False)
+        Else
+            TxtBoxProductCode.DataBindings.Add("Text", BindingSourceProducts, "ProductCode")
+            TxtBoxProductName.DataBindings.Add("Text", BindingSourceProducts, "ProductName", False, DataSourceUpdateMode.OnPropertyChanged)
+            TxtBoxProductDesc.DataBindings.Add("Text", BindingSourceProducts, "ProductDescription")
+            NumBoxProductPrice.DataBindings.Add("Value", BindingSourceProducts, "ProductPrice")
+            NumBoxProductMinQuantity.DataBindings.Add("Value", BindingSourceProducts, "ProductMinQuantity")
+        End If
+    End Sub
 
-        TxtBoxProductCode.DataBindings.Add("Text", BindingSourceProducts, "ProductCode")
-        TxtBoxProductName.DataBindings.Add("Text", BindingSourceProducts, "ProductName")
-        TxtBoxProductDesc.DataBindings.Add("Text", BindingSourceProducts, "ProductDescription")
-        NumBoxProductPrice.DataBindings.Add("Value", BindingSourceProducts, "ProductPrice")
-        NumBoxProductMinQuantity.DataBindings.Add("Value", BindingSourceProducts, "ProductMinQuantity")
+    Private Sub BtnRefreshListProducts_Click(sender As Object, e As EventArgs) Handles BtnRefreshListProducts.Click
+
+        LoadData()
     End Sub
 End Class
