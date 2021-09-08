e350e62cd12b62ad0947e24ce3fcb6be0bc5f057
Xhunik Miguel <xhuniktzi@gmail.com>
2021-09-08 16:36:43 -0600

    client form validations

--- Forms/ClientForm.vb
+++ Forms/ClientForm.vb
@@ -1,4 +1,6 @@
-﻿Public Class ClientForm
+﻿Imports System.Text.RegularExpressions
+
+Public Class ClientForm
     Private ReadOnly _viewModel As ClientMainViewModel
 
     Public Sub New()
@@ -24,6 +26,7 @@
             TxtBoxClientDirection.DataBindings.Add("Text", BindingSourceClients, "ClientDirection")
             TxtBoxClientEMail.DataBindings.Add("Text", BindingSourceClients, "ClientEMail")
         End If
+        BtnUpdateClient.Enabled = ValidateForm()
     End Sub
 
     Private Sub ClientForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -63,5 +66,41 @@
     Private Sub BtnCreateClient_Click(sender As Object, e As EventArgs) Handles BtnCreateClient.Click
         CreateClientForm.cargar = New CreateClientForm.operar(AddressOf LoadData)
         CreateClientForm.ShowDialog()
+    End Sub
+
+    Private Function ValidateForm() As Boolean
+        Dim ValidationName As Boolean = Not (String.IsNullOrEmpty(TxtBoxClientName.Text) Or String.IsNullOrWhiteSpace(TxtBoxClientName.Text))
+        Dim ValidationNit As Boolean = Not (String.IsNullOrEmpty(TxtBoxClientNit.Text) Or String.IsNullOrWhiteSpace(TxtBoxClientNit.Text))
+        Dim ValidationNit2 As Boolean = Regex.IsMatch(TxtBoxClientNit.Text, "\d+-\d")
+        Dim ValidationPhone As Boolean = Not (String.IsNullOrEmpty(TxtBoxClientPhone.Text) Or String.IsNullOrWhiteSpace(TxtBoxClientPhone.Text))
+        Dim ValidationPhone2 As Boolean = Regex.IsMatch(TxtBoxClientPhone.Text, "\d+")
+        Dim ValidationDirection As Boolean = Not (String.IsNullOrEmpty(TxtBoxClientDirection.Text) Or String.IsNullOrWhiteSpace(TxtBoxClientDirection.Text))
+        Dim ValidationEmail As Boolean = Not (String.IsNullOrEmpty(TxtBoxClientEMail.Text) Or String.IsNullOrWhiteSpace(TxtBoxClientEMail.Text))
+        Dim ValidationEmail2 As Boolean = Regex.IsMatch(TxtBoxClientEMail.Text, "\w@\w+\.\w+")
+        Return ValidationName And ValidationNit And ValidationNit2 And ValidationPhone And ValidationPhone2 And ValidationDirection And ValidationEmail And ValidationEmail2
+    End Function
+
+    Private Sub TxtBoxClientName_TextChanged(sender As Object, e As EventArgs) Handles TxtBoxClientName.TextChanged
+        BtnUpdateClient.Enabled = ValidateForm()
+    End Sub
+
+    Private Sub TxtBoxClientNit_TextChanged(sender As Object, e As EventArgs) Handles TxtBoxClientNit.TextChanged
+        BtnUpdateClient.Enabled = ValidateForm()
+    End Sub
+
+    Private Sub TxtBoxClientPhone_TextChanged(sender As Object, e As EventArgs) Handles TxtBoxClientPhone.TextChanged
+        BtnUpdateClient.Enabled = ValidateForm()
+    End Sub
+
+    Private Sub TxtBoxClientDirection_TextChanged(sender As Object, e As EventArgs) Handles TxtBoxClientDirection.TextChanged
+        BtnUpdateClient.Enabled = ValidateForm()
+    End Sub
+
+    Private Sub TxtBoxClientEMail_TextChanged(sender As Object, e As EventArgs) Handles TxtBoxClientEMail.TextChanged
+        BtnUpdateClient.Enabled = ValidateForm()
+    End Sub
+
+    Private Sub LstBoxListClients_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LstBoxListClients.SelectedIndexChanged
+        BtnUpdateClient.Enabled = ValidateForm()
     End Sub
 End Class
